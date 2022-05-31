449ce6efbcc5ed44a82631ae6f37b33c4112b1f1
DakshMalik02 <daksh0399.be21@chitkara.edu.in>
2022-05-31 22:51:05 +0530

    Input file updated using array

--- input.cpp
+++ input.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 using namespace std;
 int main() {
-int number;
-cout << "Enter an integer: ";
-cin >> number;
-cout << "You entered " << number;
+int number[5];
+// In this code I have updated it to input an array and print its members.
+cout << "Enter 5 integers: "<<endl;
+for (int i =0;i<5;i++){
+    cin>>number[i];
+}
+cout << "You entered : ";
+for (int i =0;i<5;i++){
+    cout<<number[i]<<" ";
+}
+
 return 0;
 }
